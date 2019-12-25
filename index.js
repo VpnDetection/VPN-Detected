5114318cbc6c3ea9f34a1e06e111fde56c3e1232
David <daviid.saal@gmail.com>
2019-12-25 09:39:50 +0200

    Update

--- index.js
+++ index.js
@@ -1,136 +1,94 @@
-var mySvg = document.querySelector('#my-svg');
-var snap = Snap(mySvg);
-var w = mySvg.width.baseVal.value,h = mySvg.height.baseVal.value,cx = w / 2,cy = h / 2;
-
-var radius = 100;
-var perimeter = 2 * Math.PI * radius;
-var color = '#007ac1';
-
-var circle = snap.circle(cx, cy, radius);
-var text = document.querySelector('.percent-text');
-text.style.color = color;
-
-function updateGraph(perc) {
-
-  if(document.getElementById("check4").innerHTML == 'Checking Error' && document.getElementById("check5").innerHTML == 'Checking Error'){
-  color = '#e6c300';
-  text.style.color = '#e6c300';
-}
-
-if(perc >= 0.6){
-  color = '#e60000';
-  text.style.color = color;
-}
-
-  // Reset attributes
-  circle.attr({
-	fill: 'none',
-	stroke: color,
-	strokeWidth: '0.7cm',
-	strokeDasharray: '0 ' + perimeter,
-	strokeDashoffset: perimeter * .25 });
-
-  // Animate
-  Snap.animate(0, perc, val => {
-	circle.attr({
-	  strokeDasharray: perimeter * val + ' ' + perimeter * (1 - val) });
-	text.innerHTML = Math.round(val * 100) + '%';
-  }, 2500, mina.easeinout);
-};
-
-function compareLocalTime(result) {
-  document.getElementById("percent4").innerHTML = '';
-  var ipTime, systemTime, systemHour, systemMinutes, ipTimeSplit, ipHour, ipMinutes;
-
-  ipTime = String(document.getElementById('localTime').innerHTML);
-
-  systemTime = new Date();
-  document.getElementById('date').innerHTML = systemTime;
-
-  systemHour = systemTime.getHours().toString();
-  systemHour = ("0" + systemHour).slice(-2);
-
-  systemMinutes = systemTime.getMinutes().toString();
-  systemMinutes = ("0" + systemMinutes).slice(-2);
-  
-  ipTimeSplit = ipTime.split(':');
-
-  ipHour = ipTimeSplit[0];
-  ipMinutes = ipTimeSplit[1];
-
-  var test4Element = document.getElementById("test4");
-
-
-  if(!ipTime || !systemTime || !systemHour || !systemMinutes || !ipHour || !ipMinutes){
-    test4Element.classList.add("toast--yellow");
-    document.getElementById("check4").innerHTML = 'Checking Error';
-  }
-  else{
-    if(systemMinutes == '59' && ipMinutes == '00' && ipHour == Number(systemHour)+1){
-      test4Element.classList.add("toast--green");
-      document.getElementById("check4").innerHTML = 'Succeed';
-    }
-    else{
-      if(systemHour==ipHour && (systemMinutes >= ipMinutes-2 && systemMinutes <= ipMinutes+5)){
-        test4Element.classList.add("toast--green");
-        document.getElementById("check4").innerHTML = 'Succeed';
-      }
-      else{
-        test4Element.classList.add("toast--red");
-        document.getElementById("check4").innerHTML = 'Failed';
-        document.getElementById("percent4").innerHTML = ' - 20%';
-        result += 0.2;
-      }
-    }
-  }
-  timeZone(result)
-};
+var Host = document.getElementById("Host").innerHTML;
+var BlackListIP = document.getElementById("BlackListIP").innerHTML;
+var TimeZoneTxt = document.getElementById("TimeZone").innerHTML;
+ipClient = document.getElementById("ipClient").innerHTML;
+systemTime = new Date();
+var Using;
+var Result;
+var TimeZonep;
 
 function timeZone(result){
-  document.getElementById("percent5").innerHTML = '';
-  var test5Element = document.getElementById("test5");
-  var usingElement = document.getElementById("using");
-  var systemTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  var ipTimeZone = document.getElementById("TimeZone").innerHTML;
-
-  if(!systemTimeZone || !ipTimeZone){
-    test5Element.classList.add("toast--yellow");
-    document.getElementById("check5").innerHTML = 'Checking Error';
-  }
-  else{
+    var systemTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    var ipTimeZone = document.getElementById("TimeZone").innerHTML;
+    Result = result;
+  
     if(systemTimeZone == ipTimeZone){
-      test5Element.classList.add("toast--green");
-      document.getElementById("check5").innerHTML = 'Succeed';
+        TimeZonep = 0;
     }
     else{
-      test5Element.classList.add("toast--red");
-      document.getElementById("check5").innerHTML = 'Failed';
-      document.getElementById("percent5").innerHTML = ' - 20%';
-      result += 0.2;
+        TimeZonep = 35;
+        Result += 35;
     }
+
+    if(Result >= 50){
+    Using = '• Your Using VPN!';
+    }
+    else{
+    Using = '• Your Not Using VPN.';
+    }
+    init()
   }
 
-  if(document.getElementById("check4").innerHTML == 'Checking Error' && document.getElementById("check5").innerHTML == 'Checking Error'){
-    usingElement.classList.add("usingYellow");
-    document.getElementById("using").innerHTML = 'Checking Error!';
-  }
-  else{
-    if(result >= 0.6){
-      usingElement.classList.add("usingRed");
-      document.getElementById("using").innerHTML = 'Your Using VPN!';
+
+function init(){
+  i = [0,0,0,0,0,0,0,0]
+
+  //Client Info//
+  Ip = '• Ip: ' + ipClient;
+  SysDate = '• Sys Date: ' + systemTime;
+  Timezone = '• Time Zone: ' + TimeZoneTxt;
+
+  //Fucntion//
+  BlackListip = '• BlackListIP: ' + BlackListIP +'%' + ' - Determine whether IP belongs to a hosting or VPN organization.';
+  TimeZoneResult = '• Time Zone: ' + TimeZonep +'%' + ' - Compare client time zone with his system time zone.';
+  HostField = '• "Host" Field: ' + Host +'%' + ' - Check "Host" IP header field is belongs to VPN or not.';
+
+  //Result//
+  FinalResult = '• Result: ' + Result +'%';
+
+  var speed = 150;
+
+  function typeWriter() {
+      //Client Info//
+      if (i[0] < Ip.length) {
+        document.getElementById("Ip").innerHTML += Ip.charAt(i[0]);
+        i[0]++;
+      }
+      if (i[1] < SysDate.length) {
+        document.getElementById("SysDate").innerHTML += SysDate.charAt(i[1]);
+        i[1]++;
+      }
+      if (i[2] < Timezone.length) {
+        document.getElementById("Timezone").innerHTML += Timezone.charAt(i[2]);
+        i[2]++;
+      }
+
+      //Fucntion//
+      if (i[3] < BlackListip.length) {
+        document.getElementById("BlackListip").innerHTML += BlackListip.charAt(i[3]);
+        i[3]++;
+      }
+      if (i[4] < TimeZoneResult.length) {
+        document.getElementById("TimeZoneResult").innerHTML += TimeZoneResult.charAt(i[4]);
+        i[4]++;
+      }
+      if (i[5] < HostField.length) {
+        document.getElementById("HostField").innerHTML += HostField.charAt(i[5]);
+        i[5]++;
+      }
+
+      //Result//
+      if (i[6] < FinalResult.length) {
+        document.getElementById("FinalResult").innerHTML += FinalResult.charAt(i[6]);
+        i[6]++;
+      }
+      if (i[7] < Using.length) {
+        document.getElementById("Using").innerHTML += Using.charAt(i[7]);
+        i[7]++;
+      }
+
+        setTimeout(typeWriter, speed);
     }
-    else{
-      usingElement.classList.add("usingBlue");
-      document.getElementById("using").innerHTML = 'Your Not Using VPN.';
-    }
-  }
-  updateGraph(result);
+
+    typeWriter()
 }
-
-jQuery(document).ready(function(){
-  jQuery('.toast__close').click(function(e){
-    e.preventDefault();
-    var parent = $(this).parent('.toast');
-    parent.fadeOut("slow", function() { $(this).remove(); } );
-  });
-});
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var request = require('request');
-var geoip = require('geoip-lite');
-var CountryLanguage = require('country-language');
 var bodyParser = require('body-parser')
 var app = express();
 var ipClient;
@@ -10,26 +8,21 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var result, accept_language, time, flag, time_zone;
-var country, fullCountry, answer, ans;
-var test1,test2,test3;
-var check1,check2,check3;
-var percent1,percent2,percent3;
+var result, time_zone;
+var ans;
+var percent1,percent2;
 
 app.get('/', function(req, res) {
     percent1 = null;
     percent2 = null;
-    percent3 = null;
     result = 0;
-    //console.log(JSON.stringify(req.headers));
+
     console.log("\n------------------------------------------------------");
     //ipClient = '217.182.175.75'; //Proxy
     //ipClient = '104.248.140.7'; //VPN
-    //ipClient = '109.64.87.92'; //Real IP
+    //ipClient = '109.64.101.97'; //Real IP
     ipClient = req.header('x-forwarded-for');
-    accept_language = req.header('accept-language');
-    country = geoip.lookup(ipClient)['country'];
-    fullCountry = CountryLanguage.getCountry(country).name;
+
     console.log("Client Connected..");
     console.log(`Client IP: ${ipClient}`);
     console.log("----------------BlackListIP--------------------");
@@ -38,7 +31,7 @@ app.get('/', function(req, res) {
 
 let port = process.env.PORT;;
 if (port == null || port == "") {
-    port = 8000;
+    port = 3000;
   }
 
 app.listen(port);
@@ -52,19 +45,14 @@ function BlackListIP(res)
     var bodyData1 = JSON.parse(body);
     if(error || bodyData1['message'] == 'IP Address is invalid.'){
       console.log('BlackListIP error:', bodyData1['message']);
-      test1 = 'yellow';
-      check1 = 'Checking Error';
     }
     else{
-      if(bodyData1['vpn_or_proxy'] == 'no'){
-          test1 = 'green';
-          check1 = 'Succeed';
+      if(bodyData1['risk'] == 'normal'){
+        percent1 = '0'
         }
         else{
-          test1 = 'red';
-          check1 = 'Failed';
-          result += 0.3;
-          percent1 = ' - 30%'
+          result += 50;
+          percent1 = '50'
         }
         console.log('BlackListIP result is:', bodyData1['vpn_or_proxy']);
       }
@@ -75,15 +63,12 @@ function BlackListIP(res)
 )};
 
 
-
 function HostChecker(res)
 {
    request(`https://www.ipqualityscore.com/api/json/ip/uglNEXB1BLgftt4M5FyKRFrpdRFk6t0W/${ipClient}`,function(error,response,body){
     var bodyData = JSON.parse(body);
     if(error || bodyData['success'] == false){
       console.log('HostChecker error:', bodyData['success']);
-      test2 = 'yellow';
-      check2 = 'Checking Error';
     }
     else{
       ans = 0;
@@ -91,15 +76,12 @@ function HostChecker(res)
       replace = ipNumbers[0]+'-'+ipNumbers[1]+'-'+ipNumbers[2]+'-'+ipNumbers[3];
       ipNumbers.forEach(function (num) {ans += bodyData['host'].includes(num)})
       if(bodyData['host'] == ipClient || (bodyData['region'] == 'N\/A' && bodyData['city'] == 'N\/A') || !bodyData['host'].includes(replace) || ans < 4){
-          test2 = 'red';
-          check2 = 'Failed';
-          result += 0.1;
-          percent2 = ' - 10%'
+          result += 15;
+          percent2 = '15'
         }
-        else{
-          test2 = 'green';
-          check2 = 'Succeed';
-        }
+      else{
+        percent2 = '0'
+      }
         console.log('HostChecker result is:', bodyData['host'] + ' || Region: ' + bodyData['region'] + ' || City: ' + bodyData['city']);
       }
     console.log('2. Result is:', result);
@@ -117,70 +99,7 @@ function Time_Zone(res){
     time_zone = JSON.parse(body)['timezone'];
     console.log('Time_Zone result:', time_zone);
   }  
-  console.log("------------------Country_Language--------------------");
-  Country_Language(res);
-  })
-};
-
-function Country_Language(res){
-  try {
-    console.log('iP_Country:',country);
-    console.log('Accepted_Language:', accept_language);
-  } catch (err) {'Country_Language Function Error!'; test3 = 'yellow'; check3 = 'Checking Error';}
-  request(`https://api.ipgeolocation.io/timezone?apiKey=3f643672d11b4aff9c827233f1e5cb05&tz=` + fullCountry ,function(error,response,body){
-  if(error || fullCountry == undefined){
-    time = undefined;
-  }
-  else{
-    time = JSON.parse(body)['time_24'];
-  }  
-  
-  answer = 0;
-
-  CountryLanguage.getCountryLanguages(country, function (err, languages) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      console.log('Languages: ',languages);
-      languages.forEach(function (languageCodes) {
-        console.log('#.', languageCodes.iso639_1);
-        if(languageCodes.iso639_1 == 'en'){
-        }
-        else{
-          answer += accept_language.includes(languageCodes.iso639_1);
-        }
-        })
-      }
-    })
-  console.log('Country_Language result:', answer);
-
-  if(!country || !accept_language){
-    console.log('Country_Language error: Country: ' + country + '  Accepted_Language: ' + accept_language);
-    test3 = 'yellow';
-    check3 = 'Checking Error';
-  }
-  else{
-    if(country == 'US'){
-      test3 = 'yellow';
-      check3 = 'Cannot check in US';
-    }
-    else{
-      if(answer == 0){
-        test3 = 'red';
-        check3 = 'Failed';
-        result += 0.2;
-        percent3 = ' - 20%'
-      }
-      else{
-        test3 = 'green';
-        check3 = 'Succeed';
-    }
-  }
-}
-  console.log('3. Result is:', result);
   console.log("--------------------Request End-----------------------");
-  flag = 'https://www.countryflags.io/' + country + '/shiny/24.png'
-  res.render('index',{result:result,time:time,country:fullCountry,flag:flag,time_zone:time_zone,ipClient:ipClient,test1:test1,test2:test2,test3:test3,check1:check1,check2:check2,check3:check3,percent1,percent2,percent3});
-});
+  res.render('index',{result:result,time_zone:time_zone,ipClient:ipClient,percent1,percent2});
+  })
 };
