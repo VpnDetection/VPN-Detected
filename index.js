1f33331bc8397b3381d607da91a34e12a81bf2a6
David <daviid.saal@gmail.com>
2020-01-08 18:58:32 +0200

    Update

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function timeZone(result){
 
 function isTor(result){
 
-  document.getElementById("domain").innerHTML = 'Domain:' + window.location.username;
+  document.getElementById("domain").innerHTML = 'Domain:' + document.domain;
 
   if (performance.now() % 100 !== 0) {
     Tor = 0;
